9ab2d7a575947168aa0f4950b6eaff5a7f615d33
Akila Mendis <akila@oneit.com.au>
2020-12-22 22:02:50 +0530

    9. More Props & State Work

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,14 +6,21 @@ interface ToDoItem {
 }
 interface ToDoListProps {
     items : ToDoItem[];
+    onDeleteTodo:(id:string) => void;
 }
 
-const TodoList: React.FC<ToDoListProps> = (props:any) => {
+const TodoList: React.FC<ToDoListProps> = (props:ToDoListProps) => {
     
     return(
         <ul>
             {
-                props.items.map((todo:ToDoItem) => <li key={todo.id}>{todo.text}</li>)
+                props.items.map((todo:ToDoItem) => 
+                    <li key={todo.id}>
+                        <span>{todo.text}</span>
+                        <button onClick={props.onDeleteTodo.bind(null,todo.id)}>DELETE</button>
+                    </li>
+                    
+                )
             }
         </ul>
     );
